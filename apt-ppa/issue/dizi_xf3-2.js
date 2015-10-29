0924eb786e924274ce88201398575d4790d4e35a
metinsanli <metinsanli@gmail.com>
2015-10-29 17:27:33 +0300

    bazi kurulum sorunlari, dokuman eklendi.

---
+++ apt-ppa/issue/dizi_xf3-2.js
@@ -0,0 +1,39 @@
+<script type="text/javascript">
+                    jwplayer('video').setup({
+                    width: '100%',
+                    height: '100%',
+                    autostart: 'false',
+                    repeat: 'false',
+                    fallback: 'true',
+                    primary: 'html5',
+                    wmode: 'opaque',
+            title: "",
+            sources: [
+{"file": "http://dizipub.com/player/video/aHR0cHM6Ly9yZWRpcmVjdG9yLmdvb2dsZXZpZGVvLmNvbS92aWRlb3BsYXliYWNrP3JlcXVpcmVzc2w9eWVzJmlkPTI5YmNmODFhM2IyYWFlYTAmaXRhZz0xOCZzb3VyY2U9cGljYXNhJmNtbz1zZWN1cmVfdHJhbnNwb3J0JTNEeWVzJmlwPTAuMC4wLjAmaXBiaXRzPTAmZXhwaXJlPTE0NDMwNjIyNzYmc3BhcmFtcz1yZXF1aXJlc3NsLGlkLGl0YWcsc291cmNlLGlwLGlwYml0cyxleHBpcmUmc2lnbmF0dXJlPUMzNzYyREM0MUY5OTIyN0Q2RDAzRURCMjZFNUM5MzE5NzE3NjM2QjYuOUQ5MjRDMTBBMzhEMkNDRjMwRUVGOTk0MEMyMzE0N0Q4ODNEMDI5NyZrZXk9bGgx.mp4", "label": "360", "type": "mp4"}, 
+{"file": "http://dizipub.com/player/video/aHR0cHM6Ly9yZWRpcmVjdG9yLmdvb2dsZXZpZGVvLmNvbS92aWRlb3BsYXliYWNrP3JlcXVpcmVzc2w9eWVzJmlkPTI5YmNmODFhM2IyYWFlYTAmaXRhZz0yMiZzb3VyY2U9cGljYXNhJmNtbz1zZWN1cmVfdHJhbnNwb3J0JTNEeWVzJmlwPTAuMC4wLjAmaXBiaXRzPTAmZXhwaXJlPTE0NDMwNjIyNzYmc3BhcmFtcz1yZXF1aXJlc3NsLGlkLGl0YWcsc291cmNlLGlwLGlwYml0cyxleHBpcmUmc2lnbmF0dXJlPTEzMzAyRUFBNjQyMjE5NEEwRUFCQTdDOUZENkU0NzdBNDE0QjRGMkYuNEU0NTE3MjBDRkNEMTFDQUVFQ0EyQzkwRDVGOTNBM0FFQUM0MzRDRCZrZXk9bGgx.mp4", "label": "720", "type": "mp4"}, 
+{"file": "http://dizipub.com/player/video/aHR0cHM6Ly9yZWRpcmVjdG9yLmdvb2dsZXZpZGVvLmNvbS92aWRlb3BsYXliYWNrP3JlcXVpcmVzc2w9eWVzJmlkPTI5YmNmODFhM2IyYWFlYTAmaXRhZz0zNiZzb3VyY2U9cGljYXNhJmNtbz1zZWN1cmVfdHJhbnNwb3J0JTNEeWVzJmlwPTAuMC4wLjAmaXBiaXRzPTAmZXhwaXJlPTE0NDMwNjIyNzYmc3BhcmFtcz1yZXF1aXJlc3NsLGlkLGl0YWcsc291cmNlLGlwLGlwYml0cyxleHBpcmUmc2lnbmF0dXJlPTk1NDYwQkIyRDYzMzE3QkVFMzY4RjRDNjY3MUY0QkVDOEZFQTE2N0MuRjczRUQyM0RDOUFGMERGQ0RCM0YxN0I3QUMzQ0Q2OTU1MzRDNzRFJmtleT1saDE=.3gp", "label": "180", "type": "3gp"}, 
+{"file": "http://dizipub.com/player/video/aHR0cHM6Ly9yZWRpcmVjdG9yLmdvb2dsZXZpZGVvLmNvbS92aWRlb3BsYXliYWNrP3JlcXVpcmVzc2w9eWVzJmlkPTI5YmNmODFhM2IyYWFlYTAmaXRhZz0zNyZzb3VyY2U9cGljYXNhJmNtbz1zZWN1cmVfdHJhbnNwb3J0JTNEeWVzJmlwPTAuMC4wLjAmaXBiaXRzPTAmZXhwaXJlPTE0NDMwNjIyNzYmc3BhcmFtcz1yZXF1aXJlc3NsLGlkLGl0YWcsc291cmNlLGlwLGlwYml0cyxleHBpcmUmc2lnbmF0dXJlPTkwOEI0NjJBRUEwNEIxMDc2QTdGNDRFM0IxRTQ2QjEyQzczNDkzNzguQjU4OUQ3MjE1MEY4NTkxNTI3RDMzODlCRDhGMUM2QUQxQzQ2RjcxMCZrZXk9bGgx.mp4", "label": "1080", "type": "mp4"}],            modes: [
+            { type: "flash", src: "http://dizipub.com/player/_kaynak/jwplayer.flash.swf" },
+            { type: "html5" }
+            ],
+                    logo: {
+                    file: 'http://dizipub.com/player/_kaynak/player.png',
+                            link: 'http://dizipub.com',
+                            hide: false,
+                            position: 'top-left',
+                            linktarget: '_blank'
+                    },
+                    captions: {
+                    color: '#FFFFFF',
+                            backgroundOpacity: 0,
+                            fontSize: 23,
+                            edgeStyle: 'dropshadow',
+                    },
+                    abouttext: 'Pub Player',
+                    aboutlink: 'http://dizipub.com',
+                    startparam: "start",
+                    provider: "http",
+                    width:"100%",
+                    height:"100%"
+            });
+        </script>
