8d827ab510469695e669c39899cdd1fa45524b13
metinsanli <metinsanli@gmail.com>
2017-11-10 23:19:11 +0300

    bbq10 script guncelleme

--- script/bbq10-mount.sh
+++ script/bbq10-mount.sh
@@ -3,8 +3,9 @@
 # config
 mnt_source=//192.168.1.23/media
 mnt_targt=/mnt/bbq10
-mnt_uname=Blackberry-Q10
+mnt_uname=blackberryq10
 mnt_psswd=1234567890
+local_uname=pi
 
 # begin
 if echo "$HOME" | grep '/root'
@@ -16,7 +17,7 @@ if echo "$HOME" | grep '/root'
     exit 1
    else
     echo "trying mount from "$mnt_source" to "$mnt_targt" ..."
-    mount -t cifs $mnt_source $mnt_targt -o username=$mnt_uname,passwd=$mnt_psswd,uid=pi,gid=pi
+    mount -t cifs $mnt_source $mnt_targt -o username=$mnt_uname,passwd=$mnt_psswd,uid=$local_uname,gid=$local_uname
 	if mount | grep $mnt_targt > /dev/null; then
 	 echo "success."
 	 fi
